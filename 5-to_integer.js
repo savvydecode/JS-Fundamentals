252acbf6ecafe4ec10fa18a2279215685d300259
savvycode <addostephen557@gmail.com>
2025-07-11 07:01:07 +0000

    add file: 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,16 @@
+function checkArguments() {
+    args = process.argv.slice(2); //Get arguments passed to the script, excluding the first two default ones
+    if(typeof(args) === undefined) {
+        console.log("Not a number"); 
+    } else {
+        
+        if( isNaN(args)){
+            console.log("Not a number"); 
+        
+        } else{
+            console.log(parseInt(args));
+        }
+
+    }
+}   
+checkArguments();
