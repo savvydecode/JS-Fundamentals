592e11e7c4a75cd9fb86d0e9bec220c2c70a1b27
savvycode <addostephen557@gmail.com>
2025-07-11 11:40:15 +0000

    add file; 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+
+const lang1 = "C is fun";
+const lang2 = "Python is cool";
+const lang3 = "JavaScript is amazing";
+
+const myArray = [];
+myArray[0] = lang1;
+myArray[1] = lang2;
+myArray[2] = lang3;
+
+for (i = 0; i < myArray.length; i++){
+
+    console.log(myArray[i])
+}
+
+    
+
